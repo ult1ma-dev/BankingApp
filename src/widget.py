a24a4fe21208dc217202d425b0617d45e90898f4
Максим Магазейщиков <maxan43@yandex.ru>
2026-07-25 14:14:24 +0300

    Изменение кода под критерии. Защита от некорректного ввода + datetime

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,25 @@
 """Функции для обработки банковских реквизитов."""
 
+from datetime import datetime
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_number_and_type: str) -> str:
     """Вернуть название карты или счета с замаскированным номером."""
-    card_data = card_number_and_type.split()
-    number = card_data[-1]
-    name = " ".join(card_data[:-1])
+    if not card_number_and_type.strip():
+        raise ValueError("Строка с названием и номером не должна быть пустой.")
+
+    card_data = card_number_and_type.rsplit(maxsplit=1)
+
+    if len(card_data) != 2:
+        raise ValueError("Введите название и номер через пробел.")
+
+    name = card_data[0]
+    number = card_data[1]
+
+    if not number.isdigit():
+        raise ValueError("Номер должен состоять только из цифр.")
 
     if name == "Счет":
         return name + " " + get_mask_account(number)
@@ -17,8 +29,6 @@ def mask_account_card(card_number_and_type: str) -> str:
 
 def get_date(date_string: str) -> str:
     """Вернуть дату в формате ДД.ММ.ГГГГ."""
-    year = date_string[0:4]
-    month = date_string[5:7]
-    day = date_string[8:10]
+    date = datetime.fromisoformat(date_string)
 
-    return day + "." + month + "." + year
+    return date.strftime("%d.%m.%Y")
