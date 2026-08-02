6b15930bafb07aa057a25e13c0aac5cd92baa49e
Максим Магазейщиков <maxan43@yandex.ru>
2026-08-02 11:09:52 +0300

    фиксы модуля processing

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,6 @@
-def filter_by_state(list_of_dicts: list, state = 'EXECUTED') -> list:
+def filter_by_state(list_of_dicts: list[dict[str, object]], state:str = 'EXECUTED') -> list[dict[str, object]]:
     filtered_dicts = []
-    for dict in list_of_dicts:
-        if dict['state'] == state:
-            filtered_dicts.append(dict)
+    for operation in list_of_dicts:
+        if operation['state'] == state:
+            filtered_dicts.append(operation)
+    return filtered_dicts
