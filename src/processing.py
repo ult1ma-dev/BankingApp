bc92be9d0437c9af0645688f426dbfc7837d4dff
Максим Магазейщиков <maxan43@yandex.ru>
2026-08-02 11:12:39 +0300

    Исправление processing под линтеры

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,8 @@
-def filter_by_state(list_of_dicts: list[dict[str, object]], state:str = 'EXECUTED') -> list[dict[str, object]]:
+def filter_by_state(list_of_dicts: list[dict[str, object]], state: str = "EXECUTED") -> list[dict[str, object]]:
     filtered_dicts = []
+
     for operation in list_of_dicts:
-        if operation['state'] == state:
+        if operation["state"] == state:
             filtered_dicts.append(operation)
+
     return filtered_dicts
