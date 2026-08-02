6f93684c9547fee790a8e27e7d27cdabec76af78
Максим Магазейщиков <maxan43@yandex.ru>
2026-08-02 11:43:38 +0300

    Укорачивание функций в более красивый вид

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,8 @@
 def filter_by_state(list_of_dicts: list[dict[str, object]], state: str = "EXECUTED") -> list[dict[str, object]]:
-    filtered_dicts = []
 
-    for operation in list_of_dicts:
-        if operation["state"] == state:
-            filtered_dicts.append(operation)
-
-    return filtered_dicts
+    return [operation for operation in list_of_dicts if operation["state"] == state]
 
 
 def sort_by_date(list_of_dicts: list[dict[str, object]], reverse: bool = True) -> list[dict[str, object]]:
-    sorted_dicts = sorted(
-        list_of_dicts,
-        key=lambda operation: str(operation["date"]),
-        reverse=reverse,
-    )
-    return sorted_dicts
+
+    return sorted(list_of_dicts, key=lambda operation: str(operation["date"]), reverse=reverse)
