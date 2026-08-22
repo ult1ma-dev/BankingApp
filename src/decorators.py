1ec92acf03b4b3d1ea79e5a57a92de98024d685f
Максим Магазейщиков <maxan43@yandex.ru>
2026-08-22 12:40:27 +0300

    Создание декоратора логгирования

---
+++ src/decorators.py
@@ -0,0 +1,39 @@
+import time
+from datetime import datetime
+from functools import wraps
+
+def log(filename=None):
+
+    def write_log_choice(message):
+        if filename:
+            with open(filename, 'a', encoding="utf-8") as f:
+                f.write(f"{message}\n")
+        else:
+            print(f"{message}")
+
+    def wrapper(func):
+
+        @wraps(func)
+        def inner(*args, **kwargs):
+            write_log_choice(f"{func.__name__} начала работу в {datetime.now()}")
+
+            start_time = time.perf_counter()
+            try:
+                result = func(*args, **kwargs)
+                write_log_choice(f"{func.__name__} ok")
+                write_log_choice(f"Result of {func.__name__} work: {result}")
+            except Exception as e:
+                write_log_choice(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                raise
+            finally:
+                execution_time = time.perf_counter() - start_time
+
+                write_log_choice(f"{func.__name__} с аргументами {args} и {kwargs} завершила работу в {datetime.now()}")
+                write_log_choice(f"Время выполнения: {execution_time:.6f} сек.")
+
+            return result
+
+        return inner
+
+    return wrapper
+
